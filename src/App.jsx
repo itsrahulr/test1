1432c175947561c8a033977374edfe3a51d25c71
itsrahulr <therahul27r@gmail.com>
2024-03-05 17:49:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-blue-200 text-black">Test</div>
+    </>
+  );
+}
+
+export default App;
